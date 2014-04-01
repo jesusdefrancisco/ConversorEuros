20519013954820c7a002eb5f331b861e98694a1b
jesusdefrancisco <jesus.defrancisco@uem.es>
2014-04-01 11:24:39 +0200

    tras rollback
    commit tras rollback.

--- src/conversoreuros/ConversorEuros.java
+++ src/conversoreuros/ConversorEuros.java
@@ -1,8 +1,10 @@
-/*
+﻿/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+// comentario para que elimine el rollback.
 
 package conversoreuros;
 
